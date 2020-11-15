6e526873640ac13a9699ab7736ab4bb6cfc1be01
Golodhrim <holidayninjastaff@gmail.com>
2020-11-15 06:39:47 -0500

    server file fixes and heroku packagestart scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from '../src/router/AppRouter';
+import AppRouter from './router/AppRouter';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
