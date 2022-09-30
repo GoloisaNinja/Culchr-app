684f3f4d4a2358bf44bb1235293953bea6700678
Golodhrim <holidayninjastaff@gmail.com>
2022-09-30 13:44:58 -0400

    small change to corporate phrase section

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -11,24 +11,25 @@ const Api = ({ userInput }) => {
 		synsArray: [],
 	});
 	const { thesArray, synsArray } = apiResults;
+	const inputUpper = userInput.toUpperCase();
 	const badForYou = [
-		`Implementing ${userInput} will dramatically downsize cost initiatives while pairing substantial velocity with core values.`,
-		`${userInput} is a more than a brand statement - it positions us to be more agile and averse to the adversarial.`,
-		`${userInput} allows us to think outside the box to empower our niche and utilize best practices.`,
-		`We need to shift the paradigm and ${userInput} will allow us to do that, action items are forthcoming.`,
-		`Flexing our human capital will be easier if we used ${userInput}, allowing us to drill down and identify risk.`,
-		`${userInput} is a game changer that will revolutionize our decruitment agendas and policies.`,
-		`We strongly believe that ${userInput} will allow us to synergize and homogenize earnings across our business vectors.`,
-		`Operationalizing ${userInput} will engender greater blue-sky thinking, and we anticipate marginalized net-net gain.`,
-		`Before ${userInput}, we had a siloed approach to encapsulation - ${userInput} gives us more of bleeding and leading edge.`,
-		`Let's recontextualize ${userInput}, we need to initiate a Tiger Team to evaluate and value add to the proposition.`,
-		`${userInput} allows us to ladder up to our framework and optimizes our social and moral consciousness.`,
-		`${userInput} will allow for greater actualization of our systematic and overarching deliverables towards sustainablity.`,
-		`The 30,000 foot view is that ${userInput} will foster output of 110% within core competencies and derivative actionables.`,
-		`${userInput} might feel like a big ask initially - reference the scope document and let's take any concerns offline.`,
-		`${userInput} will ultimately add value and operationally open a dialog with our points of contact, so we can be same page.`,
-		`Reducing bottlenecks with ${userInput} is our proactive stance to address more with less.`,
-		`We can seamlessly integrate ${userInput} into our value-add props which should give our change agents more bandwidth.`,
+		`Implementing ${inputUpper} will dramatically downsize cost initiatives while pairing substantial velocity with core values.`,
+		`${inputUpper} is a more than a brand statement - it positions us to be more agile and averse to the adversarial.`,
+		`${inputUpper} allows us to think outside the box to empower our niche and utilize best practices.`,
+		`We need to shift the paradigm and ${inputUpper} will allow us to do that, action items are forthcoming.`,
+		`Flexing our human capital will be easier if we used ${inputUpper}, allowing us to drill down and identify risk.`,
+		`${inputUpper} is a game changer that will revolutionize our decruitment agendas and policies.`,
+		`We strongly believe that ${inputUpper} will allow us to synergize and homogenize earnings across our business vectors.`,
+		`Operationalizing ${inputUpper} will engender greater blue-sky thinking, and we anticipate marginalized net-net gain.`,
+		`Before ${inputUpper}, we had a siloed approach to encapsulation - ${inputUpper} gives us more of bleeding and leading edge.`,
+		`Let's recontextualize ${inputUpper}, we need to initiate a Tiger Team to evaluate and value add to the proposition.`,
+		`${inputUpper} allows us to ladder up to our framework and optimizes our social and moral consciousness.`,
+		`${inputUpper} will allow for greater actualization of our systematic and overarching deliverables towards sustainablity.`,
+		`The 30,000 foot view is that ${inputUpper} will foster output of 110% within core competencies and derivative actionables.`,
+		`${inputUpper} might feel like a big ask initially - reference the scope document and let's take any concerns offline.`,
+		`${inputUpper} will ultimately add value and operationally open a dialog with our points of contact, so we can be same page.`,
+		`Reducing bottlenecks with ${inputUpper} is our proactive stance to address more with less.`,
+		`We can seamlessly integrate ${inputUpper} into our value-add props which should give our change agents more bandwidth.`,
 	];
 	// MAKE RIDICULOUS ARRAY FOR FINAL PANEL
 	let outputArr = [];
@@ -138,8 +139,8 @@ const Api = ({ userInput }) => {
 				</div>
 				<div className='defined-margin'>
 					<p className='defined-lead'>
-						<Rocket style={{ verticalAlign: 'bottom' }} size={30} /> CULCHR'IZE
-						AND INDOCTRINATE - MEANINGLESS POWER PHRASES FOR EMAILS!
+						<Rocket style={{ verticalAlign: 'bottom' }} size={30} /> CULCHR'IZED
+						CORPORATE SPEAK - COPY/PASTE MEANINGLESS POWER PHRASES FOR EMAILS!
 					</p>
 					<ul className='defined-ul'>
 						{outputArr.map((val, index) => (
